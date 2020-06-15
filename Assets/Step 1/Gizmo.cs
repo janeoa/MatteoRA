cc57df918acd39a8f9afab5e3f3104e9b3e252e9
Asset Malik <asetmalik@gmail.com>
2020-06-15 16:17:39 +0600

    Добавил Empty на каждый bone для pivot
    У всех bone стоял pivot на orgin, по этому они неправильно крутились. Добавил кучу пустых обьектов выше по иерархии и поставил их на правильные позиции pivot чтобы можно было спокойно крутить. Такое должно было делатся прямо в CAD, но не смотря на то что у модели был уже готовый bone system и даже анимация прямо в fbx, почему-то unity правильно импортировать bone не смог. Пока что обойдусь этим методом, но если нужно будет, то лучше поставить правилньый pivot через CAD, а не как я, на глаз в Unity

---
+++ Assets/Step 1/Gizmo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gizmo : MonoBehaviour
+{
+    public float gizmosize = 0.75f;
+    public Color gizmoColor = Color.yellow;
+
+    void OnDrawGizmos() {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawWireSphere(transform.position, gizmosize);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
