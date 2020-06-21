5227047b0a35c0b744287fb250c5fc7fcd5d31b3
Asset Malik <asetmalik@gmail.com>
2020-06-21 14:05:34 +0600

    Massive upadte
    * Lighting
    * backgroud items
    * Better shadows

---
+++ Assets/Step2/negativeBiasShadow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class negativeBiasShadow : MonoBehaviour
+{
+    public Light lght;
+    public float setBias = -1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lght.shadowBias = setBias;
+    }
+}
